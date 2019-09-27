36057f79569fb17d52048590f228c4f481c0f115
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-27 10:38:38 -0600

    part 1 complete

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const endpoint = "https://dog.ceo/api/breeds/image/random/50";
 
 
-function getImages() {
+function getImages(num) {
   fetch('https://dog.ceo/api/breeds/image/random/50')
+    .then(res => {
+      //console.log(res);
+      return res.json()
+    })
+    .then(resData => extractImages(resData, num))
+    .catch(error => console.log(error));
 }
+
+
+function extractImages(resData, num) {
+  let {
+    message,
+  } = resData
+  for (let i=0; i < num; i++){
+    console.log(message[i]);
+  }
+}
+
+function submitButton() {
+  $('.searchDogImages').submit(function(event) {
+    event.preventDefault();
+    let numberInput = $('.userInputNumber').val();
+    getImages(numberInput);
+  });
+}
+
+$(submitButton);
